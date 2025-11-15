dd7d79cb5ce5cd831d3749c81a8e4ef69d7987d1
Prashantism12 <pashu.s.madival@gmail.com>
2025-11-15 11:14:32 +0530

    save

--- scores.py
+++ scores.py
@@ -11,8 +11,13 @@ else:
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
+
 
 print("Scores:", scores)
 print("Sum of scores:", total)
 print("Average score:", average)
+print("Maximum score:", maximum)
+print("Minimum score:", minimum)
 
